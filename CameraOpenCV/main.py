ea40b5c5bcc9e69a5dbc789f2293b6353e4f6ac1
Evgeniy Galiullin <zhenya-skyline@mail.ru>
2026-02-15 17:50:09 +0500

    добавлена обработка изображения и видео

--- CameraOpenCV/main.py
+++ CameraOpenCV/main.py
@@ -1,16 +1,55 @@
-# This is a sample Python script.
+from pickletools import uint8
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import cv2
+import numpy as np
+
+#создаем матрицу(изображение) со значениями 0
+photo = np.zeros((300, 300, 3), dtype='uint8')
+
+# #показываем изображение (photo[0::100, 0::150] - обрезать изображение)
+cv2.imshow('Photo', photo)
+#время показа изображения
+cv2.waitKey(0)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# #читаем изображение
+# img = cv2.imread('images/cat.jpg')
+# #меняем размер изображения
+# img = cv2.resize(img, (350, 500))
+# #степень размытия (только нечетные числа)
+# img = cv2.GaussianBlur(img, (9, 9),0)
+# #конвертация изображения в черно-белый цвет
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# #найти углы изображения (уменьшаем точность)
+# img = cv2.Canny(img, 100, 100)
+#
+# #матрица избражения со значением 1
+# kernel = np.ones((5, 5), np.uint8)
+# #увеличение обводки
+# img = cv2.dilate(img, kernel, iterations=1)
+# #уменьшить жирность обводки
+# img = cv2.erode(img, kernel, iterations=1)
+#
+# #показываем изображение (img[0::100, 0::150] - обрезать изображение)
+# cv2.imshow('result', img)
+# #вывести параметры изображения
+# #print(img.shape)
+#
+# #время показа изображения
+# cv2.waitKey(0)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# #читаем видео (0 - для обращения к видеокамере)
+# cap = cv2.VideoCapture('videos/butterfly.mp4')
+# #ширина (для видеокамеры)
+# cap.set(3, 500)
+# #высота (для видеокамеры)
+# cap.set(4,300)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#цикл видео
+# while True:
+#     success, img = cap.read()
+#     cv2.imshow('result', img)
+#
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
