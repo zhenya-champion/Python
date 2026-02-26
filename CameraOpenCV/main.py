addf7b85aca403ae916fb25d09c180ca790d72fd
Evgeniy Galiullin <zhenya-skyline@mail.ru>
2026-02-26 21:49:07 +0500

    Распознавание лиц, Обработка изображений и видео

--- CameraOpenCV/main.py
+++ CameraOpenCV/main.py
@@ -1,15 +1,39 @@
-from pickletools import uint8
-
 import cv2
 import numpy as np
 
+#Распознавание номерных знаков и их чтение
+
+#Распознавание лиц
+# img = cv2.imread('images/people_2.jpg')
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#
+# face = cv2.CascadeClassifier('face.xml')
+# result = face.detectMultiScale(gray, scaleFactor=1.03, minNeighbors=5) #координаты найденых объектов
+#
+# for(x, y, w, h) in result:
+#     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), thickness=3)
+#
+# cv2.imshow('Result', img)
+# cv2.waitKey(0)
+
+
 #создаем матрицу(изображение) со значениями 0
-photo = np.zeros((300, 300, 3), dtype='uint8')
+#photo = np.zeros((300, 300, 3), dtype='uint8')
+
+#photo[:] = 255, 0, 0
+#Создание прямоугольника
+#cv2.rectangle(photo, (50, 70), (100, 100), (119, 201, 105), thickness=cv2.FILLED)
+#Создание линии
+#cv2.line(photo, (0, photo.shape[0] // 2), (photo.shape[1], photo.shape[0] // 2), (119, 201, 105), thickness=3)
+#Создание круга
+#cv2.circle(photo, (photo.shape[0] // 2, photo.shape[1] // 2), 50, (119, 201, 105), thickness=cv2.FILLED)
+#Создание текста
+#cv2.putText(photo, 'Evgeniy', (100, 150), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 0, 0), 3)
 
 # #показываем изображение (photo[0::100, 0::150] - обрезать изображение)
-cv2.imshow('Photo', photo)
+#cv2.imshow('Photo', photo)
 #время показа изображения
-cv2.waitKey(0)
+#cv2.waitKey(0)
 
 
 # #читаем изображение
@@ -53,3 +77,68 @@ cv2.waitKey(0)
 #
 #     if cv2.waitKey(1) & 0xFF == ord('q'):
 #         break
+
+#читаем изображение
+#img = cv2.imread('images/cat.jpg')
+#new_image = np.zeros(img.shape, dtype='uint8')
+#отзеркаливание
+#img = cv2.flip(img, -1)
+
+#вращение
+# def rotate(img_param, angle):
+#     h, w = img_param.shape[:2]
+#     point = (w // 2, h // 2)
+#
+#     mat = cv2.getRotationMatrix2D(point, angle,1)
+#     return cv2.warpAffine(img_param, mat, (w, h))
+#img = rotate(img, -90)
+
+#смещение
+# def transform(img_param ,x ,y):
+#     mat = np.float32([[1, 0, x],[0, 1, y]])
+#     return cv2.warpAffine(img_param, mat, (img_param.shape[1], img_param.shape[0]))
+# img = transform(img, 20, 30)
+
+#контуры
+#img = cv2.GaussianBlur(img, (5, 5),0) #размытие
+#img = cv2.Canny(img, 100, 140) #углы изображения
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #конвертация
+
+#con, hir = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE) #поиск контуров
+#рисуемновое изображение
+#cv2.drawContours(new_image, con, -1, (0, 200, 0), 1)
+
+#print(con)
+
+#цветовые форматы
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+#img = cv2.cvtColor(img, cv2.COLOR_LAB2BGR)
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+#разбить на слои
+#r, g, b = cv2.split(img)
+#объединить слои
+#img = cv2.merge([b, g, r])
+
+#показываем изображение (img[0::100, 0::150] - обрезать изображение)
+#cv2.imshow('result', img)
+#время показа изображения
+#cv2.waitKey(0)
+
+#Побитовые операции
+#photo = cv2.imread('images/cat.jpg')
+#img = np.zeros(photo.shape[:2], dtype='uint8')
+
+#circle = cv2.circle(img.copy(), (200, 300), 120, 255, -1)
+#square = cv2.rectangle(img.copy(), (25, 25), (250, 350), 255, -1)
+
+#объединяем изображение
+#img = cv2.bitwise_and(photo, photo, mask=circle)
+#img = cv2.bitwise_or(circle, square)
+#img = cv2.bitwise_xor(circle, square)
+#img = cv2.bitwise_n(circle)
+
+#показываем изображение (img[0::100, 0::150] - обрезать изображение)
+#cv2.imshow('result', img)
+#время показа изображения
+#cv2.waitKey(0)
