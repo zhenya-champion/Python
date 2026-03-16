05f37830301b298f8d094c10c2252ef61e667456
Evgeniy Galiullin <zhenya-skyline@mail.ru>
2026-03-17 01:38:54 +0500

    Распознавание номеров

--- CameraOpenCV/main.py
+++ CameraOpenCV/main.py
@@ -5,7 +5,50 @@ import easyocr
 from matplotlib import pyplot as pl
 
 #Распознавание номерных знаков и их чтение
+img = cv2.imread('images/car4.jpeg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+img_filter = cv2.bilateralFilter(gray,11, 15, 15)
+edges = cv2.Canny(img_filter, 30, 200)
+
+cont = cv2.findContours(edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cont = imutils.grab_contours(cont)
+#сортируем, нужные нам контуры вверху, макс 8
+cont = sorted(cont, key=cv2.contourArea, reverse=True)#[:8]
+
+#найдём контур отображающий номер
+pos = None
+for c in cont:
+    approx = cv2.approxPolyDP(c, 10, True)
+
+    if len(approx) == 4:
+        pos = approx
+        break
+
+mask = np.zeros(gray.shape, np.uint8)
+new_img = cv2.drawContours(mask, [pos], 0, 255, -1)
+bitwise_img = cv2.bitwise_and(img, img, mask=mask)
+
+#вырезть номер
+(x, y) = np.where(mask==255)
+(x1, y1) = (np.min(x), np.min(y))
+(x2, y2) = (np.max(x), np.max(y))
+crop = gray[x1:x2, y1:y2]
+
+#считать номер
+text = easyocr.Reader(['en'])
+text = text.readtext(crop)
+#print(text)
+#pl.imshow(cv2.cvtColor(crop, cv2.COLOR_BGR2RGB))
+#pl.show()
+
+#выводим надпись на изображении
+res = text[0][-2]
+final_image = cv2.putText(img, res, (x1 - 200, y2 + 160), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 2)
+final_image = cv2.rectangle(img, (x1, x2), (y1, y2), (0, 255, 0), 2)
+
+pl.imshow(cv2.cvtColor(final_image, cv2.COLOR_BGR2RGB))
+pl.show()
 
 #Распознавание лиц
 # img = cv2.imread('images/people_2.jpg')
